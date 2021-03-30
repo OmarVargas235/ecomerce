2c571dad7599a9def5bf80dd182cd2a741e28c20
OmarVargas235 <ov1356272@gmail.com>
2021-03-30 09:59:29 -0400

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './assets/bootstrap.css';
+import Navbar from './components/navbar/';
+import Header from './components/header/';
+
+const App = () => {
+	
+	return (
+		<React.Fragment>
+			<Navbar />
+			<Header />
+		</React.Fragment>
+	)
+}
+
+export default App;
