16a84240516085096472ad3a626663e0c3d468d8
OmarVargas235 <ov1356272@gmail.com>
2021-05-21 13:46:52 -0400

    Agregando la funcion 'requestWithToken' en 'fetch'

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -18,3 +18,26 @@ export const requestWithoutToken = async (url = "", formData={}, method="GET") =
 		return data;
 	}
 }
+
+export const requestWithToken = async (url = "", formData={}, method="GET", token="") => {
+
+	// if (method === 'GET') {
+
+	// 	return fetch(`http://localhost:5000/${url}`);
+
+	// } else {
+
+		const resp = await fetch(`http://localhost:5000/${url}`, {
+			method,
+			headers: {
+		      // 'Content-Type' : 'multipart/form-data',
+		      'x-token': token
+			},
+			body: formData,
+			// body: JSON.stringify(formData),
+		});
+		const data = await resp.json();
+
+		return data;
+	// }
+}
